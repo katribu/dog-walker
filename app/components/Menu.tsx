2dcbade8a33de5dcae335bcb13a45f22fd420d45
Kat <katrinaburwash_17@hotmail.com>
2023-06-12 10:30:07 +0200

    working on menu

--- app/components/Menu.tsx
+++ app/components/Menu.tsx
@@ -1,6 +1,7 @@
 'use client'
 import { HiMenu } from "react-icons/hi";
 import {useState} from 'react'
+import Link from "next/link";
 
 export default function Menu() {
     const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false)
@@ -9,17 +10,16 @@ export default function Menu() {
         setIsMenuOpen(prevState => !prevState)
     }
     return (
-        <div onClick={handleClick} className="cursor-pointer">
+        <div onClick={handleClick} className="cursor-pointer overflow-hidden">
             <div>
                 <HiMenu />
             </div>
             {isMenuOpen && 
-            <div>
-                <ul>
-                    <li>Profile</li>
-                    <li>Home</li>
-                    <li>Help</li>
-                </ul>
+            <div className="overflow-hidden hidden absolute z-1">
+                <Link href="#">Profile</Link>
+                <Link href="#">Home</Link>
+                <Link href="#">Log Out</Link>
+                
             </div>}
         </div>
     )
